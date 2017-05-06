9b404626bea1ba7058839fcff9b05f675d55bdbc
cindy <cindialy@gmail.com>
2017-05-05 23:27:18 -0400

    Avanzando en el grupo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,36 @@
+
+var grupo= [];
+
 function Squad(nombre,edad,hobbies){
 	this.nombre= nombre;
 	this.edad= edad;
 	this.hobbies=hobbies;
-	this.persona= function(){
-       return ("<br><b> Nombre: </b> " + this.nombre + "<br><b>Edad: </b>" + this.edad + "<br><b>Hobbie: </b>" +"<br><li>" + this.hobbies + "</li>");
-	}
 }
 
-var espe = new Squad('Esperanza Lucero','26','Fotografia','Diseñar');
-var fer = new Squad('Fernanda Zamora','30', 'Escuchar música','fotografia');
-var natalia = new Squad('Natalia Villalobos','30', 'Tocar bateria');
-var vale = new Squad('Valentina Miranda','26','Dibujar', 'ver anime', 'cocinar');
+function xikas(){
+var espe = new Squad('Esperanza Lucero','26',['Fotografia','Diseñar','Dormir']);
+var fer = new Squad('Fernanda Zamora','30', ['Escuchar música','Fotografia','Pensar en el existencialismo']);
+var natalia = new Squad('Natalia Villalobos','30',['Tocar bateria', 'Hacer queques','Dormir']);
+var vale = new Squad('Valentina Miranda','26',['Dibujar', 'Ver anime', 'Cocinar']);
+var diana= new Squad('Diana Villasmil','29', ['Ver series Coreanas', 'Cantar', 'Escuchar música']);
+var cindy= new Squad('Cindialy Berrios', '23',['Trekking','Deporte', 'Escuchar musica']);
 
+grupo.push(espe);
+grupo.push(fer);
+grupo.push(natalia);
+grupo.push(vale);
+grupo.push(diana);
+grupo.push(cindy);
+};
 
-var per1= espe.persona();
-var per2= fer.persona();
-var per3= natalia.persona();
-var per4= vale.persona();
+xikas();
 
-var todas = per1 + per2 + per3 + per4;
+grupo.forEach(function(per){
+	var presentacion="<br><b> Nombre: </b> " + per.nombre + "<br><b>Edad: </b>" + per.edad + "<br><b>Hobbies: </b>";
 
-document.getElementById('persona').innerHTML = todas;
+per.hobbies.forEach(function(hobbie){
+	presentacion += "<ul><li>" + hobbie + "</li></ul>";
+});
+
+document.getElementById('persona').innerHTML += presentacion ;
+});
