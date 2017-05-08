96e1c8797b833154ce183a8f7ddab377976e59b7
cindy <cindialy@gmail.com>
2017-05-07 22:04:42 -0400

    agregando funciones para comentarios y likes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,36 @@
+var fotos=[];
 
-var grupo= [];
+	fotos["Fernanda Zamora"]= "fer.jpg";
+	fotos["Natalia Villalobos"]= "natalia.jpg";
+  fotos["Diana Villasmil"]= "diana.jpg";
 
-function Squad(nombre,edad,hobbies){
+
+class Squad {
+
+	constructor(nombre, edad, hobbies) {
+
+  this.imagen= new Image();
 	this.nombre= nombre;
 	this.edad= edad;
 	this.hobbies=hobbies;
+
+	this.imagen.src=fotos[this.nombre];
+   }
+
 }
 
-function xikas(){
-var espe = new Squad('Esperanza Lucero','26',['Fotografia','Diseñar','Dormir']);
-var fer = new Squad('Fernanda Zamora','30', ['Escuchar música','Fotografia','Pensar en el existencialismo']);
-var natalia = new Squad('Natalia Villalobos','30',['Tocar bateria', 'Hacer queques','Dormir']);
-var vale = new Squad('Valentina Miranda','26',['Dibujar', 'Ver anime', 'Cocinar']);
-var diana= new Squad('Diana Villasmil','29', ['Ver series Coreanas', 'Cantar', 'Escuchar música']);
-var cindy= new Squad('Cindialy Berrios', '23',['Trekking','Deporte', 'Escuchar musica']);
+var grupo= [];
 
-grupo.push(espe);
-grupo.push(fer);
-grupo.push(natalia);
-grupo.push(vale);
-grupo.push(diana);
-grupo.push(cindy);
-};
+    grupo.push(new Squad('Esperanza Lucero','26',['Fotografia','Diseñar','Dormir']));
+    grupo.push( new Squad('Fernanda Zamora','30', ['Escuchar música','Fotografia','Pensar en el existencialismo']));
+    grupo.push( new Squad('Natalia Villalobos','30',['Tocar bateria', 'Hacer queques','Dormir']));
+    grupo.push( new Squad('Valentina Miranda','26',['Dibujar', 'Ver anime', 'Cocinar']));
+    grupo.push(new Squad('Diana Villasmil','29', ['Ver series Coreanas', 'Cantar', 'Escuchar música']));
+    grupo.push(new Squad('Cindialy Berrios', '23',['Trekking','Deporte', 'Escuchar musica']));
 
-xikas();
 
 grupo.forEach(function(per){
-	var presentacion="<br><b> Nombre: </b> " + per.nombre + "<br><b>Edad: </b>" + per.edad + "<br><b>Hobbies: </b>";
+	var presentacion= "<br><b> Nombre: </b> " + per.nombre + "<br><b>Edad: </b>" + per.edad +" años" + "<br><b>Hobbies: </b>";
 
 per.hobbies.forEach(function(hobbie){
 	presentacion += "<ul><li>" + hobbie + "</li></ul>";
@@ -34,3 +38,30 @@ per.hobbies.forEach(function(hobbie){
 
 document.getElementById('persona').innerHTML += presentacion ;
 });
+
+
+class Comentario {
+
+	constructor(id_miembro, comentario, likes) {
+
+
+		this.id_miembro = id_miembro;
+		this.comentario = comentario;
+		this.likes = likes;
+	}
+
+};
+var i = 0;
+
+function meGusta() {
+    i+=1;
+  document.getElementById("cuenta").innerHTML= i;
+};
+
+
+function agregarComentario(){
+
+  res= document.getElementById("comentar").value;
+  document.getElementById("mostrarComentario").innerHTML+= res + "<br>";
+	res= document.getElementById("comentar").value= "";
+};
